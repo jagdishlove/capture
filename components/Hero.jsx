74e542d629f3066f0c417034bcd44884bd642f8a
jagdish <jagdish.funn@gmail.com>
2023-03-11 23:32:15 +0530

    hero style

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 function Hero() {
   return (
     <div className='flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img' >
-        <div/>
-        <div>
-         <h2>Heading</h2>
-         <p>Message</p>
-         <button>Book</button>
+        <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/20 z-[2] '/>
+        <div className='p-5 text-white x-[2] '>
+         <h2 className='font-bold text-5xl'>Heading</h2>
+         <p className='py-5 text-xl'>Message</p>
+         <button className='px-8 py2 border'>Book</button>
         </div>
     </div>
   )
