ef4217fce3d496ae99494b16bad6cda92ea139e0
jagdish <jagdish.funn@gmail.com>
2023-03-11 22:53:20 +0530

    updated

---
+++ components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img' >
+        <div/>
+        <div>
+         <h2>Heading</h2>
+         <p>Message</p>
+         <button>Book</button>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
